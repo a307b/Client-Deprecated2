960c3b7f89c1b357f903e7af4bf0b8e1a18c20f3
Peter K <pkjar16@student.aau.dk>
2018-03-15 13:46:20 +0100

    Update Main.java

--- blockchain/src/main/java/Main.java
+++ blockchain/src/main/java/Main.java
@@ -5,7 +5,5 @@ public class Main
     public static void main(String[] args) throws IOException
     {
         OSCheck.updateIPFS();
-        IpfsCore ipfsCore = new IpfsCore();
-        ipfsCore.connect();
     }
 }
