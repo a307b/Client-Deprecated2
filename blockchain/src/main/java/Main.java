c85fd2183917bd16875bf4614c9a5e4ba9660036
sava17 <37406543+sava17@users.noreply.github.com>
2018-03-15 14:43:37 +0100

    Bedre kommentar

--- blockchain/src/main/java/Main.java
+++ blockchain/src/main/java/Main.java
@@ -5,6 +5,6 @@ public class Main
     public static void main(String[] args) throws IOException
     {
         OSCheck.updateIPFS();
-		//Samuel69_pussyslayer_69
+		//Kristoffer69_pussyslayer_69
     }
 }
