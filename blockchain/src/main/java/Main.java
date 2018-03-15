364bed8598d3c5787969af5071d509c7de4d1abe
kris914g <Kristoffer@aagard.dk>
2018-03-15 22:20:51 +0100

    Testing javaFX

--- blockchain/src/main/java/Main.java
+++ blockchain/src/main/java/Main.java
@@ -1,11 +1,37 @@
 import java.io.IOException;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
-public class Main
+public class Main extends Application
 {
+
+    Button button;
+
     public static void main(String[] args) throws IOException
     {
         //OSCheck.updateIPFS();
-        Frame gui = new Frame();
-        gui.setVisible(true);
+        launch(args);
     }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception
+    {
+        primaryStage.setTitle("Login"); //Title
+        button = new Button();
+        button.setText("Click me");
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+
+        Scene scene = new Scene(layout, 300,250);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+
 }
